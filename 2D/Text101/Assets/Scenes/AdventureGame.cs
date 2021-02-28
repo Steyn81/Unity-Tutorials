fee8d369bdef8adf98789e2345b10eebfedc98d4
Deon Steyn <32790719+Steyn81@users.noreply.github.com>
2021-02-28 08:46:07 +0200

    Add Text101 Project

---
+++ 2D/Text101/Assets/Scenes/AdventureGame.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Needed for Text
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    //Will keep track of which state we are in
+    State state;
+
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
+}
+//States and State Machines
+//--------------------------------------------------------------------------------
+//State = Action/Process/Behaviour (the thing that we are doing usually.)
+//State Machine assumes only 1 state at a time.(but can have many states)
+//Conditions (requirements) transition from one state to the next.
+
+//Example of State Machine
+//Start => State A => Decision? => (Decision 1) State B    / Start => Room with 2 Exits => Which Way? => (Blue Door) Real World
+//                              => (Decision 2) State C     /                                          => (Red Door) Fantasy Land
+
+//Scriptable Object
+//--------------------------------------------------------------------------------
+//ScriptableObject It's a class that lets us store data in stand alone assets
+//Keep mountains of data out of our scripts
+//It's lightweight and convenient
+//Used as a template for consistency
+
+//              Our Code                                    Scriptable Objects
+//  ------------------------------                      ----------------------
+//  |       AdventureGame.cs     |             ||=====  |    Story Data A    |
+//  ------------------------------             ||       ----------------------
+//  |  |Display Story Text: |    |             ||       ---------------------- 
+//  ------------------------------             ||=====  |    Story Data B    |
+//  |       |Story Data C |      |  <==========||       ---------------------- 
+//  |       ---------------      |             ||       ---------------------- 
+//  |                            |             ||=====  |    Story Data C    |
+//  ------------------------------                      ---------------------- 
+
+// Create a new Scriptable Object
+//---------------------------------------------------------------------------------
+// Add new script => open new script => change MonoBehaviour to ScriptableObject
---
+++ 2D/Text101/Assets/Scenes/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//add new option to unity menu structure (R-Click in unity)
+[CreateAssetMenu (menuName = "State")] 
+public class State : ScriptableObject
+{
+    //[TextArea(min size of field in the inspector, amount of lines before scroll]
+    [TextArea(10,14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
