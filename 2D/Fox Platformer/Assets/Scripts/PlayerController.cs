876022c695a4b6d496380b48b058823146e39ddd
Deon Steyn <32790719+Steyn81@users.noreply.github.com>
2021-03-04 18:46:53 +0200

    Add Player and Movement

---
+++ 2D/Fox Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 7.5f;
+    [SerializeField] Rigidbody2D rb;
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        rb.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rb.velocity.y);
+    }
+}
