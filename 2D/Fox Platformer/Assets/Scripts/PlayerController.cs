0f7be3f944c1c6a525138479c0b12939cd55d2ea
Deon Steyn <32790719+Steyn81@users.noreply.github.com>
2021-03-08 10:40:51 +0200

    Add Player movement, Jump and Animation

--- 2D/Fox Platformer/Assets/Scripts/PlayerController.cs
+++ 2D/Fox Platformer/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,61 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 7.5f;
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] float jumpForce;
+    [SerializeField] Transform groundCheckPoint;
+    [SerializeField] LayerMask whatIsGround;
+
+    private bool isGrounded;
+    private bool canDoubleJump;
+
+    private Animator anim;
+    private SpriteRenderer sr;
     
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
 
     void Update()
     {
-        rb.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rb.velocity.y);
+        rb.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), rb.velocity.y); //GetAxisRaw - stop movement immediately
+
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position,.2f,whatIsGround);
+
+        if(isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+            else
+            {
+                if(canDoubleJump)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
+            
+        }
+
+        if(rb.velocity.x < 0)
+        { 
+            sr.flipX = true; 
+        }
+        else if(rb.velocity.x > 0)
+        {
+            sr.flipX = false;
+        }
+
+        anim.SetFloat("moveSpeed", Mathf.Abs(rb.velocity.x)); //always returns a positive number
+        anim.SetBool("isGrounded", isGrounded);
     }
 }
